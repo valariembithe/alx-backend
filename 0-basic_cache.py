f96b37aa1cac9c5f1be9f8b15cd0512f732c5349
valariembithe <valmbithe691@gmail.com>
2024-01-31 11:27:13 +0300

    Files

---
+++ 0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env pyhton3
+""" Create a class BasicCache"""
+from base_caching import BaseCaching
+
+
+class BasicCache:
+    """Returns data from parent class attribute cache-data"""
+    def put(self, key, item):
+        """Assigns the item value to the key"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Returns the value linked to key"""
+        if key is None or key != isinstance(self.cache_data.keys()):
+            return self.cache_data.get(key, None)
+        return self.cache_data.get(key)
+    
