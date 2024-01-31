5210a43115e2d330b89c6b007c5b4208b6027600
valariembithe <valmbithe691@gmail.com>
2024-01-31 11:55:06 +0300

    Task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Task 1 module """\
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''A class that inherits from BaseCaching'''
+    def __init__(self):
+        '''Initialize the attributes from parent class'''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        ''' USing FIFO to pop the first item to cache '''
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data.items()) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print('Discard: {}'.format(first_key))
+
+    def get(self, key):
+        '''Return the value linked to the key popped'''
+        if key is None or key != self.cache_data.keys():
+            return self.cache_data.get(key, None)
+        return self.cache_data.get(key)
