93e7b1ed7d597d8b3ead89f2a21f117c64613b47
valariembithe <valmbithe691@gmail.com>
2024-01-31 13:01:37 +0300

    Task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python3
-''' Last In First Out Cache module'''
+''' LRU Cache module'''
 from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
-    '''Implements LIFO in caching'''
+class LRUCache(BaseCaching):
+    ''' Implements a class that uses LRU for caching'''
     def __init__(self):
         ''' Initializes attributes from parent class'''
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        ''' Implements LIFO method in caching'''
+        '''Implements the least recently used method'''
         if key is None or item is None:
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", last_key)
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=True)
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         '''Return the value linked to the key popped'''
         if key is None or key != self.cache_data.keys():
             return self.cache_data.get(key, None)
         return self.cache_data.get(key)
+    
