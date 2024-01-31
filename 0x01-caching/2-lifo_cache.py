5a648358d5544fdb252af33e6ce9e0ed35be8353
valariembithe <valmbithe691@gmail.com>
2024-01-31 12:26:17 +0300

    Task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
 ''' Last In First Out Cache module'''
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    ''''''
+    '''Implements LIFO in caching'''
+    def __init__(self):
+        ''' Initializes attributes from parent class'''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        ''' Implements LIFO method in caching'''
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if key not in self.cache_data:
+            if len(self.cache_data.items()) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        '''Return the value linked to the key popped'''
+        if key is None or key != self.cache_data.keys():
+            return self.cache_data.get(key, None)
+        return self.cache_data.get(key)
+
+
