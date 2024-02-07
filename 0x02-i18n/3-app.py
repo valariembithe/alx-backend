75d0d7012067d1d4cdde8bca4fd1b897f996fd8e
valariembithe <valmbithe691@gmail.com>
2024-02-07 11:25:00 +0300

    Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''Task 3 module'''
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+class Config:
+    '''Configure languages available for app'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    '''Determines best mach with supported languages'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def get_index() -> str:
+    ''' Returns index template'''
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
