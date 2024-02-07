3694033f53e05a7e249dd4d26ff7b5a507632dd2
valariembithe <valmbithe691@gmail.com>
2024-02-07 12:22:41 +0300

    Task 5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+'''Task 5 module'''
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import g, request
+from typing import Union, Dict
+
+
+class Config:
+    '''Configure languages available for app'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    '''Returns user dict if logged in'''
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+@app.before_request
+def before_request() -> None:
+    '''Before a request is made'''
+    user = get_user()
+    g.user = user
+
+@babel.localeselector
+def get_locale() -> str:
+    '''Determines best mach with supported languages'''
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def get_index() -> str:
+    ''' Returns index template'''
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
